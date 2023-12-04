867c78ede47aab33442d24f12ee9a0e63a0f983f
muneebshahzad <77743328+muneebshahzad@users.noreply.github.com>
2023-12-05 03:40:45 +0500

    Update shopifyApi.js

--- netlify/functions/shopifyApi.js
+++ netlify/functions/shopifyApi.js
@@ -4,8 +4,8 @@ const fetch = require('node-fetch');
 
 exports.handler = async function (event, context) {
   const apiKey = 'a8e7646c131a57f30477a97d2cdde5bb';
-  const apiSecret = 'YOUR_SHOPIFY_API_SECRET';
-  const shopifyApiUrl = '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';
+  const apiSecret = 'c25a5316dd408414932ef4c72c377c2b';
+  const shopifyApiUrl = 'https://alkaramat.myshopify.com/admin/api/2023-10/graphql.json';
 
   const graphqlQuery = `
     {
